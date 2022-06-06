bf208e93c0e1546b59ebb62b465147f7fc1cc527
Andy Cary <info@andycary.com>
2022-06-06 09:31:04 -0400

    create dropdown directive to toggle button menu

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -11,6 +11,7 @@ import { RecipeDetailComponent } from './components/recipe-detail/recipe-detail.
 import { RecipeItemComponent } from './components/recipe-item/recipe-item.component';
 import { ShoppingListComponent } from './components/shopping-list/shopping-list.component';
 import { ShoppingEditComponent } from './components/shopping-edit/shopping-edit.component';
+import { DropdownDirective } from './shared/dropdown.directive';
 
 @NgModule({
   declarations: [
@@ -21,7 +22,8 @@ import { ShoppingEditComponent } from './components/shopping-edit/shopping-edit.
     RecipeDetailComponent,
     RecipeItemComponent,
     ShoppingListComponent,
-    ShoppingEditComponent
+    ShoppingEditComponent,
+    DropdownDirective
   ],
   imports: [
     BrowserModule,
---
+++ src/app/shared/dropdown.directive.ts
@@ -0,0 +1,13 @@
+import { Directive, HostBinding, HostListener } from "@angular/core";
+
+@Directive({
+    selector: '[appDropdown]',
+    exportAs: 'appDropdown'
+})
+export class DropdownDirective {
+    @HostBinding('class.dropdown-toggle') isOpen = false;  
+
+    @HostListener('click') toggleOpen() {
+        this.isOpen = !this.isOpen;
+    }
+}
