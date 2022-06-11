8a398cbe792f8581702a6a5f98f7898f77ab380d
Andy Cary <info@andycary.com>
2022-06-11 10:09:03 -0400

    add footer and new route for shopping list

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -4,12 +4,14 @@ import { AppComponent } from './app.component';
 import { UsersComponent } from './components/users/users.component';
 import { HomeComponent } from './components/home/home.component';
 import { RecipesComponent } from './components/recipes/recipes.component';
+import { ShoppingListComponent } from './components/shopping-list/shopping-list.component';
 
 const routes: Routes = [
   //{ path: '', pathMatch: 'full', redirectTo: 'home' },
   { path: '', component: HomeComponent },
   { path: 'home', component: HomeComponent },
   { path: 'recipes', component: RecipesComponent },
+  { path: 'shopping', component: ShoppingListComponent },
   { path: 'users', component: UsersComponent },
   { path: '**', component: AppComponent }
 ];
