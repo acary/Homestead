531f12eb98de8a882b15e26c2a72da002afd24f8
Andy Cary <info@andycary.com>
2022-06-10 21:09:57 -0400

    add ingredients to recipe model, add ingredients to shopping list from recipe detail

--- src/app/components/recipe-detail/recipe-detail.component.ts
+++ src/app/components/recipe-detail/recipe-detail.component.ts
@@ -1,4 +1,6 @@
 import { AfterViewInit, Component, Input, OnInit, ViewChild } from '@angular/core';
+import { ShoppingListService } from 'src/app/services/shopping-list.service';
+import { Ingredient } from 'src/app/shared/ingredient.model';
 import { MakeThisComponent } from '../make-this/make-this.component';
 import { Recipe } from '../recipes/recipe.model';
 
@@ -7,17 +9,24 @@ import { Recipe } from '../recipes/recipe.model';
   templateUrl: './recipe-detail.component.html',
   styleUrls: ['./recipe-detail.component.css']
 })
+
 export class RecipeDetailComponent implements OnInit, AfterViewInit {
   @Input() recipe: Recipe;
   @ViewChild(MakeThisComponent) makeThis: MakeThisComponent; 
 
-  constructor() { }
+  constructor(
+    private shoppingListService: ShoppingListService
+  ) { }
 
   ngOnInit(): void {
   }
 
   ngAfterViewInit() {
     console.log(this.makeThis.attach());
+  }
+
+  onAddToShoppingList(ingredients: Ingredient[]) {
+    this.shoppingListService.addIngredients(ingredients);
   }
 
 }
--- src/app/services/recipe.service.ts
+++ src/app/services/recipe.service.ts
@@ -1,12 +1,33 @@
 import { EventEmitter } from "@angular/core";
 import { Recipe } from "../components/recipes/recipe.model";
+import { Ingredient } from "../shared/ingredient.model";
 
 export class RecipeService {
     recipeSelected = new EventEmitter<Recipe>();
 
     private recipes: Recipe[] = [
-        new Recipe('Craft Burger', 'Delicious Summer Hamburger', 'assets/images/burger.jpeg'),
-        new Recipe('Baked Beans', 'Homestyle Beans', 'assets/images/beans.jpeg'),
+        new Recipe(
+            'Craft Burger', 
+            'Delicious Summer Hamburger', 
+            'assets/images/burger.jpeg',
+            [
+                new Ingredient('Beef', 1),
+                new Ingredient('Cheese', 1),
+                new Ingredient('Lettuce', 1),
+                new Ingredient('Pickles', 1),
+                new Ingredient('Onion', 1),
+                new Ingredient('Bun', 2)
+            ]
+            ),
+        new Recipe(
+            'Baked Beans', 
+            'Homestyle Beans', 
+            'assets/images/beans.jpeg',
+            [
+                new Ingredient('Beans', 1),
+                new Ingredient('Onion', 1)
+            ]
+            ),
     ];
 
     getRecipes() {
