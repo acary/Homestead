6d47b154221a34e853f93a2d62d037e66dbbd9b2
Andy Cary <info@andycary.com>
2022-06-16 05:27:25 -0400

    start editing: click item gets ingredient by index from service, sets Value in form using ViewChild

--- src/app/components/shopping-edit/shopping-edit.component.ts
+++ src/app/components/shopping-edit/shopping-edit.component.ts
@@ -1,5 +1,7 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnDestroy, OnInit, ViewChild } from '@angular/core';
 import { NgForm } from '@angular/forms';
+import { Subscription } from 'rxjs';
+
 import { ShoppingListService } from 'src/app/components/shopping-list/shopping-list.service';
 import { Ingredient } from 'src/app/shared/ingredient.model';
 
@@ -8,19 +10,40 @@ import { Ingredient } from 'src/app/shared/ingredient.model';
   templateUrl: './shopping-edit.component.html',
   styleUrls: ['./shopping-edit.component.css']
 })
-export class ShoppingEditComponent implements OnInit {
+export class ShoppingEditComponent implements OnInit, OnDestroy {
+  @ViewChild('f', { static: false }) slForm: NgForm;
+  subscription: Subscription;
+  editMode = false;
+  editedItemIndex: number;
+  editedItem: Ingredient;
 
   constructor(
     private shoppingListService: ShoppingListService
   ) { }
 
   ngOnInit(): void {
+    this.shoppingListService.startedEditing
+      .subscribe(
+        (index: number) => {
+          this.editedItemIndex = index;
+          this.editMode = true;
+          this.editedItem = this.shoppingListService.getIngredient(index);
+          this.slForm.setValue({
+            name: this.editedItem.name,
+            amount: this.editedItem.amount
+          })
+        }
+      );
   }
 
   onAddItem(form: NgForm) {
     const value = form.value;
     const newItem = new Ingredient(value.name, value.amount);
     this.shoppingListService.addIngredient(newItem);
+  }
+
+  ngOnDestroy() {
+      this.subscription.unsubscribe();
   }
 
 }
--- src/app/components/shopping-list/shopping-list.service.ts
+++ src/app/components/shopping-list/shopping-list.service.ts
@@ -3,6 +3,7 @@ import { Ingredient } from "../../shared/ingredient.model";
 
 export class ShoppingListService {
     ingredientsChanged = new Subject<Ingredient[]>();
+    startedEditing = new Subject<number>();
 
     private ingredients = [
         new Ingredient('Beef', 1),
@@ -11,6 +12,10 @@ export class ShoppingListService {
 
     getIngredients() {
         return this.ingredients.slice();
+    }
+
+    getIngredient(index: number) {
+        return this.ingredients[index];
     }
 
     addIngredient(ingredient: Ingredient) {
