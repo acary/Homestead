2152ed7172c2190bee701cb3fef9b47dcc8275e5
Andy Cary <info@andycary.com>
2022-07-07 05:33:31 -0400

    split services into core module

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,31 +1,23 @@
-import { HttpClientModule, HTTP_INTERCEPTORS } from '@angular/common/http';
+import { HttpClientModule } from '@angular/common/http';
 import { NgModule } from '@angular/core';
 import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { AuthInterceptorService } from './components/auth/auth-interceptor.service';
 import { AuthComponent } from './components/auth/auth.component';
-import { IdeaService } from './components/ideas/idea.service';
+import { ActiveUsersComponent } from './components/users/active-users/active-users.component';
+import { CoreModule } from './core.module';
 import { IdeasComponent } from './components/ideas/ideas.component';
+import { InactiveUsersComponent } from './components/users/inactive-users/inactive-users.component';
 import { FooterComponent } from './components/navigation/footer/footer.component';
 import { HeaderComponent } from './components/navigation/header/header.component';
 import { NotFoundComponent } from './components/not-found/not-found.component';
-import { RecipeService } from './components/recipes/recipe.service';
 import { RecipesModule } from './components/recipes/recipes.module';
 import { ShoppingListModule } from './components/shopping-list/shopping-list.module';
-import { ShoppingListService } from './components/shopping-list/shopping-list.service';
-import { ActiveUsersComponent } from './components/users/active-users/active-users.component';
-import { AuthGuard } from './components/users/auth-guard.service';
-import { AuthService } from './components/users/auth.service';
-import { InactiveUsersComponent } from './components/users/inactive-users/inactive-users.component';
 import { SubscriptionListComponent } from './components/users/subscription-list/subscription-list.component';
-import { UsersComponent } from './components/users/users.component';
-import { UsersService } from './components/users/users.service';
-import { CounterService } from './services/counter.service';
-import { LoggingInterceptorService } from './services/logging-interceptor.service';
 import { SharedModule } from './shared/shared.module';
+import { UsersComponent } from './components/users/users.component';
 
 @NgModule({
   declarations: [
@@ -43,23 +35,13 @@ import { SharedModule } from './shared/shared.module';
   imports: [
     AppRoutingModule,
     BrowserModule,
+    CoreModule,
     FormsModule,
     HttpClientModule,
     RecipesModule,
     ReactiveFormsModule,
     ShoppingListModule,
     SharedModule
-  ],
-  providers: [
-    { provide: HTTP_INTERCEPTORS, useClass: AuthInterceptorService, multi: true },
-    { provide: HTTP_INTERCEPTORS, useClass: LoggingInterceptorService, multi: true },
-    AuthGuard,
-    AuthService,
-    CounterService,
-    IdeaService,
-    RecipeService,
-    ShoppingListService,
-    UsersService
   ],
   bootstrap: [AppComponent]
 })
---
+++ src/app/core.module.ts
@@ -0,0 +1,29 @@
+import { HTTP_INTERCEPTORS } from "@angular/common/http";
+import { NgModule } from "@angular/core";
+import { AuthInterceptorService } from "./components/auth/auth-interceptor.service";
+import { AuthGuard } from "./components/auth/auth.guard";
+import { AuthService } from "./components/auth/auth.service";
+import { IdeaService } from "./components/ideas/idea.service";
+import { RecipeService } from "./components/recipes/recipe.service";
+import { ShoppingListService } from "./components/shopping-list/shopping-list.service";
+import { UsersService } from "./components/users/users.service";
+import { CounterService } from "./services/counter.service";
+import { LoggingInterceptorService } from "./services/logging-interceptor.service";
+
+@NgModule({
+    providers: [
+        { provide: HTTP_INTERCEPTORS, useClass: AuthInterceptorService, multi: true },
+        { provide: HTTP_INTERCEPTORS, useClass: LoggingInterceptorService, multi: true },
+        AuthGuard,
+        AuthService,
+        CounterService,
+        IdeaService,
+        RecipeService,
+        ShoppingListService,
+        UsersService
+    ],
+    declarations: [],
+    imports: [],
+    exports: []
+})
+export class CoreModule { }
